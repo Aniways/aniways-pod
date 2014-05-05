27e58504eee1a37ed140056ab94fa02e0ccea1ae
Danny Shmueli <dannyshmueli@gmail.com>
2014-05-05 16:37:19 +0300

    added binary that uses AFNetworking

---
+++ Aniways.framework/Versions/A/Headers/AWCoolFetcher.h
@@ -0,0 +1,15 @@
+//
+//  AWCoolFetcher.h
+//  Aniways
+//
+//  Created by Danny Shmueli on 5/5/14.
+//  Copyright (c) 2014 Ram Greenberg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AWCoolFetcher : NSObject
+
+-(void)fetch;
+
+@end
