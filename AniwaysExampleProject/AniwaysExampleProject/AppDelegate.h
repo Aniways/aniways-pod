3929e0d7e32c162c82e4c54525b53fa59458be85
Danny Shmueli <dannyshmueli@gmail.com>
2014-11-24 00:30:54 +0200

    added example project

---
+++ AniwaysExampleProject/AniwaysExampleProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AniwaysExampleProject
+//
+//  Created by Danny Shmueli on 11/23/14.
+//  Copyright (c) 2014 Aniways. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AniwaysExampleProject/AniwaysExampleProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AniwaysExampleProject
+//
+//  Created by Danny Shmueli on 11/23/14.
+//  Copyright (c) 2014 Aniways. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
